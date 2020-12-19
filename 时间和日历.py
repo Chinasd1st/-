0f140e18d488c2895df020de98a0b4c596dc2017
Chinasd1st <68810179+Chinasd1st@users.noreply.github.com>
2020-12-19 16:35:45 +0800

    Add files via upload

---
+++ 时间和日历.py
@@ -0,0 +1,10 @@
+import calendar
+cal = calendar.month(2020, 12)
+print ("以下输出2020年12月份的日历:")
+print (cal)
+import time
+print('现在时间为：')
+print (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
+
+
